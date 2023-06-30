9e2ba90bf5433b8a22c6b838dd8883672d639f45
Richard Johansson <ricjo462@student.liu.se>
2023-06-30 13:55:39 +0300

    Added skeleton for Sign In button but no providers created yet

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,21 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react"
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
+
+  // Sign in with google and nextauth
+  const [providers, setProviders] = useState(null); 
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    }
+
+    setProviders();
+  }, [])
+  
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -47,7 +61,16 @@ const Nav = () => {
           </div>
         ): (
           <>
-          
+            {providers && Object.values(providers).map((provider) => (
+              <button 
+                type="button"
+                key={provider.name}
+                onClick={() => signIn(provider.id)}
+                className="black_btn"
+              >
+                Sign In
+              </button>
+            ))}
           </>
         )}
       </div>
