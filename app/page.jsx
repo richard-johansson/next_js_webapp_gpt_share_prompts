4d23a255f05cd097d0e748abbc75964cb0ce0b83
Richard Johansson <ricjo462@student.liu.se>
2023-06-30 13:14:21 +0300

    Started on homepage, added heading and description

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,20 @@
-
-
 const Home = () => {
+  // Tailwind CSS and styles from global.css
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+          AI-Powered Prompts
+        </span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
+      </p>
+
+      {/* Feed */}
+    </section>
   )
 }
 
