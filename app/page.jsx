e097e20217a5c30f177f68aecd6b42246854c443
Richard Johansson <ricjo462@student.liu.se>
2023-06-30 13:19:55 +0300

    Added components

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
   // Tailwind CSS and styles from global.css
   return (
@@ -13,7 +15,8 @@ const Home = () => {
         Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
       </p>
 
-      {/* Feed */}
+
+      <Feed />
     </section>
   )
 }
