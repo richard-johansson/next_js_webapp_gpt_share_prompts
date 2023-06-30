fb29b9ce5e5096524632a14ea5a5b53bb5e9687b
Richard Johansson <ricjo462@student.liu.se>
2023-06-30 14:13:40 +0300

    Added 'create prompt' and 'sign out' buttons to mobile navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -98,6 +98,23 @@ const Nav = () => {
                 >
                   My Profile
                 </Link>
+                <Link
+                  href="/create-promt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
               </div>
             )}
           </div>
